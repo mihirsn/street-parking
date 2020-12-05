d14c11f8c94964c2255a0cb9137b93543db67c2c
Mihir Naik <mihir_naik@ymail.com>
2020-12-06 01:23:55 +0530

    Add djangorestframework and corresponding changes in settings

--- street_parking_reservation/street_parking_reservation/settings.py
+++ street_parking_reservation/street_parking_reservation/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'users',
     'parking_reservations',
 ]
@@ -120,3 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [],
+    'TEST_REQUEST_DEFAULT_FORMAT': 'json'
+}

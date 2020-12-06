2a4308ea8c7b1f01078558f0f97b8b20f2ce9c8e
Mihir Naik <mihir_naik@ymail.com>
2020-12-06 12:22:08 +0530

    Add test and view to add user

--- street_parking_reservation/users/tests.py
+++ street_parking_reservation/users/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
 
-# Create your tests here.
+class CreateNewUserTest(TestCase):
+    """ Test module for inserting a new User """
+
+    def setUp(self):
+        self.valid_payload = {
+            'name': 'Bob',
+            'phone_number': '1234567890'
+        }
+        self.invalid_payload = {
+            'name': 'Alice',
+        }
+
+    def test_create_valid_user(self):
+        response = client.post(
+            reverse('get_post_puppies'),
+            data=json.dumps(self.valid_payload),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_create_invalid_user(self):
+        response = client.post(
+            reverse('get_post_puppies'),
+            data=json.dumps(self.invalid_payload),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
--- street_parking_reservation/users/views.py
+++ street_parking_reservation/users/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Users
+from .serializers import UsersSerializer
 
-# Create your views here.
+@api_view(['GET', 'POST'])
+def get_post_users(request):
+    # get all users
+    if request.method == 'GET':
+        puppies = Users.objects.all()
+        serializer = UsersSerializer(puppies, many=True)
+        return Response(serializer.data)
+    # insert a new record for a user
+    if request.method == 'POST':
+        data = {
+            'name': request.data.get('name'),
+            'phone_number': request.data.get('phone_number')
+        }
+        serializer = UsersSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)

8c8654aeab2bb2e924377c524b7f87cb98190e17
Mihir Naik <mihir_naik@ymail.com>
2020-12-06 12:22:28 +0530

    Update urls

--- street_parking_reservation/street_parking_reservation/urls.py
+++ street_parking_reservation/street_parking_reservation/urls.py
@@ -15,7 +15,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include, url
+from users import views
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('users/', )
+# ]
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^', include('users.urls')),
+    url(
+        r'^api-auth/',
+        include('rest_framework.urls', namespace='rest_framework')
+    ),
+    url(r'^admin/', admin.site.urls)
 ]

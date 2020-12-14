8cb3b8c0ca97bd48974bc3f4e889a143ae4716e9
Mihir Naik <mihir_naik@ymail.com>
2020-12-14 10:45:04 +0530

    Add unique constraint on phone number field

--- street_parking_reservation/users/models.py
+++ street_parking_reservation/users/models.py
@@ -5,6 +5,6 @@ class Users(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.TextField()
     password = models.CharField(max_length=50)
-    phone_number = models.CharField(max_length=12)
+    phone_number = models.CharField(max_length=12, unique = True)
     created_on = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
